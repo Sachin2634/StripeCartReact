0b38887647cfa82995dac6f569ba9909cd172559
Sachin2634 <105629187+Sachin2634@users.noreply.github.com>
2025-03-15 11:11:46 +0530

    a small update

--- petstore/src/pages/Store.jsx
+++ petstore/src/pages/Store.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { productsArray } from '../productsStore'
-import ProductCard from '../components/ProductCard'
+import ProductCard from '../components/productCard'
 
 const Store = () => {
   return (
